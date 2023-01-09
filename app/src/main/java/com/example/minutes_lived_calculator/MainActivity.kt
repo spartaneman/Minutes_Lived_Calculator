3518a008a9ea5a1d75f5d5e10c192731f619fd33
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-01-09 14:18:37 -0800

    Initial commit

---
+++ app/src/main/java/com/example/minutes_lived_calculator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.minutes_lived_calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
