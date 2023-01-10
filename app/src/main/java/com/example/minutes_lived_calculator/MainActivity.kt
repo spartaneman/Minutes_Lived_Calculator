9e2c40609bfbfccebce0bca72d987c220ab6e117
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-01-09 17:49:26 -0800

    Application is complete

--- app/src/main/java/com/example/minutes_lived_calculator/MainActivity.kt
+++ app/src/main/java/com/example/minutes_lived_calculator/MainActivity.kt
@@ -28,41 +28,56 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun clickDatePicker(){
+    private fun clickDatePicker(){
 
         val myCalendar = Calendar.getInstance() //part of Java.util
         val year = myCalendar.get(Calendar.YEAR)
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
 
-        //This will bring up the calender in the application and when you click on it, it will
-        //execute what is onDateSetListener
-        DatePickerDialog(this,
-            DatePickerDialog.OnDateSetListener{view,selectedYear,selectedMonth,selectedDayOfMOnth ->
-                Toast.makeText(this,"Year: $selectedYear, Month: ${selectedMonth+1}, Day: $selectedDayOfMOnth", Toast.LENGTH_LONG).show()
-                val selectedDate = "$selectedDayOfMOnth/${selectedMonth+1}/$selectedYear"
-                tvSelectedDate?.text = selectedDate
+        //We can assign a val as the datePickerDialog
+        val dpd = //This will bring up the calender in the application and when you click on it, it will
+            //execute what is onDateSetListener
+            DatePickerDialog(this,
+                DatePickerDialog.OnDateSetListener{view,selectedYear,selectedMonth,selectedDayOfMOnth ->
+                    Toast.makeText(this,"Year: $selectedYear, Month: ${selectedMonth+1}, Day: $selectedDayOfMOnth", Toast.LENGTH_LONG).show()
+                    val selectedDate = "$selectedDayOfMOnth/${selectedMonth+1}/$selectedYear"
+                    tvSelectedDate?.text = selectedDate
 
-                //This will create a SimpleDateFormat with the pattern and locale provide
-                //With this SDF we can then get a date
-                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
-                val theDate = sdf.parse(selectedDate)
+                    //This will create a SimpleDateFormat with the pattern and locale provide
+                    //With this SDF we can then get a date
+                    val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                    val theDate = sdf.parse(selectedDate)
+                    //This makes sure the date is safe and not empty
+                    //helps prevent crashing
+                    theDate?.let {
+                        //This will return the number of milliseconds that have passed since 1970
+                        //To get it into minutes we must divide by 60000
+                        val selectedDateInMinutes = theDate.time/60000
 
-                //This will return the number of milliseconds that have passed since 1970
-                //To get it into minutes we must divide by 60000
-                val selectedDateInMinutes = theDate.time/60000
+                        //get the current date in milliseconds
+                        val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                        //still must get time
+                        currentDate?.let {
+                            val currentMinutes = currentDate.time/60000
 
-                //get the current date in milliseconds from 1970
-                val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
-                val currentMinutes = currentDate.time/60000
+                            //calculate different in minutes
+                            val minutesPassed = currentMinutes - selectedDateInMinutes
 
-                val minutesPassed = currentMinutes - selectedDateInMinutes
-                tvTotalMinutes?.text = minutesPassed.toString()
+                            tvTotalMinutes?.text = minutesPassed.toString()
+                        }
+
+                    }
 
 
-            },
-            year, month,day).show()
 
+                },
+                year, month,day)
+
+        //By make the date picker a variable, we can use methods on it.
+        //In this case I limit the max Date to be the current date.
+        dpd.datePicker.maxDate = System.currentTimeMillis()
+        dpd.show()
 
     }
 }
