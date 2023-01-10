7246801404f0ee58f58cd05fa80d3a3dc56646b4
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-01-09 16:25:20 -0800

    added onClickListener

--- app/src/main/java/com/example/minutes_lived_calculator/MainActivity.kt
+++ app/src/main/java/com/example/minutes_lived_calculator/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.minutes_lived_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker: Button = findViewById(R.id.dateBtn)
+
+        btnDatePicker.setOnClickListener {
+            Toast.makeText(this,"Date picker button pressed", Toast.LENGTH_LONG).show()
+        }
     }
 }
